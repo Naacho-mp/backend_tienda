586ddaad5adef37fc04e6ec1a23cba45e5faaa8c
Nacho <nachomatamala@gmail.com>
2025-05-30 18:54:56 -0400

    Se inicia el proyecto

---
+++ README.md
@@ -0,0 +1,15 @@
+# PASOS PARA INICIALIZAR EL PROYECTO
+
+1.- Crear tu propio Entorno Virtual (venv)
+
+2.- Instalar las Librerias y Dependencias en el archivo adjunto "Requirements.txt"
+
+3.- Crear archivo .env en la raiz del Proyecto con sus credenciales de la BD:
+
+    - DB_NAME=tienda_online
+    - DB_USER=tu_usuario
+    - DB_PASSWORD=tu_pass
+    - DB_HOST=localhost
+    - DB_PORT=3306
+
+4.- Correr el Proyecto con "Python manage.py runserver"
---
+++ backend/tienda/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import redirect, render
+from tienda.models import Producto
+from tienda.forms import ProductoForm
+from django.forms import modelform_factory
+from django.core.paginator import Paginator
+
+
+ProductoForm = modelform_factory(Producto, exclude=[])
+
+
+def mostrar_productos(request):
+    productos = Producto.objects.all()
+
+    items_por_pagina = request.GET.get('paginador', 12)
+
+    page_number = request.GET.get('page')
+
+    paginador = Paginator(productos, items_por_pagina)
+    page_obj = paginador.get_page(page_number)
+
+    return render(request, 'mostrar_productos.html', {'page_obj': page_obj,})
+
+
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        producto_form = ProductoForm(request.POST, request.FILES)
+
+        if producto_form.is_valid():
+            producto_form.save()
+            return redirect('mostrar_productos')
+
+    else:
+        producto_form = ProductoForm()
+        
+    data = {'producto_form':producto_form}
+    return render(request, "agregar_producto.html", data)
+
+
+def actualizar_producto():
+    pass
+
+def eliminar_producto():
+    pass
