6e195d04f300ca2b4b399a74d349fa578a987eaa
Nicklix121q <andrediaz1234512345@gmail.com>
2025-06-02 01:16:40 -0400

    modificaciones de carrito funcional y que todos los botones de carrito estan conectados

--- backend/tienda/views.py
+++ backend/tienda/views.py
@@ -3,6 +3,7 @@ from tienda.models import Producto, Categoria
 from tienda.forms import ProductoForm, CategoriaForm
 from django.forms import modelform_factory
 from django.core.paginator import Paginator
+from django.views.decorators.http import require_POST
 
 
 ProductoForm = modelform_factory(Producto, exclude=[])
@@ -68,8 +69,52 @@ def eliminar_producto(request, producto_id):
     producto.delete()
     return redirect('mostrar_productos')
 
-def carrito(request):
-    pass
+def agregar_al_carrito(request, producto_id):
+    carrito = request.session.get('carrito', {})
+    cantidad = int(request.POST.get('cantidad', 1))
+
+    if str(producto_id) in carrito:
+        carrito[str(producto_id)] += cantidad
+    else:
+        carrito[str(producto_id)] = cantidad
+
+    request.session['carrito'] = carrito
+    return redirect('ver_producto', producto_id=producto_id)
+
+def ver_carrito(request):
+    carrito = request.session.get('carrito', {})
+
+    productos = Producto.objects.filter(id__in=carrito.keys())
+
+    carrito_detalle = []
+    total = 0 
+    for producto in productos:
+        cantidad = carrito[str(producto.id)]
+        subtotal = producto.precio * cantidad
+        total += subtotal  
+
+        carrito_detalle.append({
+            'producto': producto,
+            'cantidad': cantidad,
+            'subtotal': subtotal
+        })
+    return render(request, 'ver_carrito.html', {
+        'carrito': carrito_detalle,
+        'total': total  
+    })
+
+
+@require_POST
+def eliminar_del_carrito(request, producto_id):
+    carrito = request.session.get('carrito', {})
+
+    if str(producto_id) in carrito:
+        del carrito[str(producto_id)]
+
+    request.session['carrito'] = carrito
+    return redirect('ver_carrito')
+
+
 
 ########################## CRUD CATEGORIA ####################################
 
