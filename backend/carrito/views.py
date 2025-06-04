da114d9fb5280116b69987e07e4cfcd499986fe3
Nacho <nachomatamala@gmail.com>
2025-06-04 15:32:52 -0400

    se arregla bug cantidad del carrito y stock

--- backend/carrito/views.py
+++ backend/carrito/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from tienda.models import Producto
 from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 def agregar_al_carrito(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+
+    stock = producto.stock
     carrito = request.session.get('carrito', {})
     cantidad = int(request.POST.get('cantidad', 1))
 
-    if str(producto_id) in carrito:
-        carrito[str(producto_id)] += cantidad
-    else:
-        carrito[str(producto_id)] = cantidad
-
+    cantidad_actual = carrito.get(str(producto_id), 0)
+    nueva_cantidad = cantidad_actual + cantidad
+    
+    if nueva_cantidad > stock:
+        messages.error(request, f"No hay suficiente stock disponible del producto: '{producto.nombre}'")
+        return redirect('ver_producto', producto_id = producto_id)
+    
+    carrito[str(producto_id)] = nueva_cantidad
     request.session['carrito'] = carrito
-    return redirect('ver_producto', producto_id=producto_id)
+
+    messages.success(request, f"Se agregaron {cantidad} '{producto.nombre}' al carrito")
+    return redirect('ver_producto', producto_id = producto_id)
+
+
 
 def ver_carrito(request):
     carrito = request.session.get('carrito', {})
-
     productos = Producto.objects.filter(id__in=carrito.keys())
-
+    
     carrito_detalle = []
     total = 0 
     for producto in productos:
